c63ba3b3855728f7d04d34f7770fbc3bfc077f1c
Rick Bradley <rick@rickbradley.com>
2009-03-13 18:26:45 -0500

    Minimal show view for pages.
    Closes:  http://www.pivotaltracker.com/story/show/482250

---
+++ spec/views/pages/show.html.haml_spec.rb
@@ -0,0 +1,22 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe 'pages/show' do
+  before :each do
+    assigns[:page] = @page = Page.new(:title => 'test title', :contents => 'test contents')
+  end
+  
+  def do_render
+    render 'pages/show'
+  end
+
+  it 'should include the page title' do
+    do_render
+    response.should have_text(Regexp.new(Regexp.escape@page.title))
+  end
+  
+  it 'should include the page contents' do
+    do_render
+    response.should have_text(Regexp.new(Regexp.escape@page.contents))
+  end
+
+end
