e7f75aa7c20cb6cd1e278b48ee37bdd3750b4190
Yossef Mendelssohn <ymendel@pobox.com>
2009-03-25 16:31:44 -0500

    Refactoring pages/show view to use partial.
    Partial will also be used for previewing.

---
+++ spec/views/pages/_show.html.haml_spec.rb
@@ -0,0 +1,25 @@
+require File.expand_path(File.join(File.dirname(__FILE__), *%w[.. .. spec_helper]))
+
+describe 'pages/_show' do
+  before :each do
+    @page = Page.new(:title => 'test title', :contents => 'test contents')
+  end
+  
+  def do_render
+    render :partial => 'pages/show', :locals => { :page => @page }
+  end
+
+  it 'should include the page contents' do
+    do_render
+    response.should have_text(Regexp.new(Regexp.escape(@page.contents)))
+  end
+
+  it 'should format the page contents with markdown' do
+    @page.contents = "
+ * whatever
+ * whatever else
+"
+    do_render
+    response.should have_tag('li', :text => /whatever/)
+  end
+end
