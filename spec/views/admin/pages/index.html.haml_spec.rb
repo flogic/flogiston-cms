1292299c851d46a83a8baaa75a3e9e15d888f2f5
Yossef Mendelssohn <ymendel@pobox.com>
2009-03-26 09:13:30 -0500

    Cleaning up admin page index + spec.
    It's a lot easier to test some index things if you check all of a single row,
    then indicate there should be a row for every object.
    
    Also, the headers should be in thead, not tbody.

--- spec/views/admin/pages/index.html.haml_spec.rb
+++ spec/views/admin/pages/index.html.haml_spec.rb
@@ -4,48 +4,103 @@ describe 'admin/pages/index' do
   before :each do
     Page.delete_all
     @pages = []
-    1.upto(3) do |i|
-      @pages << Page.generate!(:title => "test title #{i}")
-    end
-    assigns[:pages] = @pages
+    @page = Page.generate!(:title => 'test title')
+    assigns[:pages] = [@page]
   end
 
   def do_render
     render 'admin/pages/index'
   end
-
-  it 'should include the page title for each page' do
+  
+  it 'should include a page list' do
     do_render
-    @pages.each do |page|
-      response.should have_text(Regexp.new(Regexp.escape(page.title)))
-    end
+    response.should have_tag('table[id=?]', 'pages')
   end
-
-  it 'should include the page handle for each page' do
-    do_render
-    @pages.each do |page|
-      response.should have_text(Regexp.new(Regexp.escape(page.handle)))
+  
+  describe 'page list' do
+    it 'should have a row for the page' do
+      do_render
+      response.should have_tag('table[id=?]', 'pages') do
+        with_tag('tbody') do
+          with_tag('tr')
+        end
+      end
     end
-  end
-
-  it 'should include an edit link for each page' do
-    do_render
-    @pages.each do |page|
-      response.should have_tag('a[href=?]', edit_admin_page_path(page))
+    
+    it 'should include the page title' do
+      do_render
+      response.should have_tag('table[id=?]', 'pages') do
+        with_tag('tbody') do
+          with_tag('tr', Regexp.new(Regexp.escape(@page.title)))
+        end
+      end
     end
-  end
-
-  it 'should include a show link for each page which opens the page in a new window' do
-    do_render
-    @pages.each do |page|
-      response.should have_tag('a[href=?][target=?]:not([onclick*=?])', '/'+page.handle, '_blank', 'delete')
+    
+    it 'should include the page handle' do
+      do_render
+      response.should have_tag('table[id=?]', 'pages') do
+        with_tag('tbody') do
+          with_tag('tr', Regexp.new(Regexp.escape(@page.handle)))
+        end
+      end
     end
-  end
-
-  it 'should include a delete link for each page' do
-    do_render
-    @pages.each do |page|
-      response.should have_tag('a[href=?][onclick*=?]', admin_page_path(page), 'delete')
+    
+    it 'should include an edit link' do
+      do_render
+      response.should have_tag('table[id=?]', 'pages') do
+        with_tag('tbody') do
+          with_tag('tr') do
+            with_tag('a[href=?]', edit_admin_page_path(@page))
+          end
+        end
+      end
+    end
+    
+    it 'should include a delete link' do
+      do_render
+      response.should have_tag('table[id=?]', 'pages') do
+        with_tag('tbody') do
+          with_tag('tr') do
+            with_tag('a[href=?][onclick*=?]', admin_page_path(@page), 'delete')
+          end
+        end
+      end
+    end
+    
+    it 'should include a show link which opens the page in a new window' do
+      do_render
+      response.should have_tag('table[id=?]', 'pages') do
+        with_tag('tbody') do
+          with_tag('tr') do
+            with_tag('a[href=?][target=?]:not([onclick*=?])', '/'+@page.handle, '_blank', 'delete')
+          end
+        end
+      end
+    end
+    
+    it 'should contain a row for each page' do
+      other_page = Page.generate!(:title => 'Some Other Title')
+      assigns[:pages].push(other_page)
+      
+      do_render
+      response.should have_tag('table[id=?]', 'pages') do
+        with_tag('tbody') do
+          [@page, other_page].each do |page|
+            with_tag('tr', Regexp.new(Regexp.escape(page.title)))
+          end
+        end
+      end
+    end
+    
+    it 'should contain no rows if there are no pages' do
+      assigns[:pages] = []
+      
+      do_render
+      response.should have_tag('table[id=?]', 'pages') do
+        with_tag('tbody') do
+          without_tag('tr')
+        end
+      end
     end
   end
 end
