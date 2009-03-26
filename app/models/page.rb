bda9461dff2066f456bd5f834c5960f00cf5df24
Yossef Mendelssohn <ymendel@pobox.com>
2009-03-25 20:52:58 -0500

    Having the Page class check if a given handle collides with routes.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,12 @@
 class Page < ActiveRecord::Base
   validates_uniqueness_of :handle
+  
+  class << self
+    def valid_handle?(handle)
+      recog = ActionController::Routing::Routes.recognize_path(handle)
+      !!(recog[:controller] == 'pages' && recog[:path])
+    rescue ActionController::RoutingError
+      true
+    end
+  end
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -44,4 +44,50 @@ describe Page do
       dup.errors.should be_invalid(:handle)
     end
   end
+  
+  describe 'as a class' do
+    it 'should be able to check if a given handle is valid' do
+      Page.should respond_to(:valid_handle?)
+    end
+    
+    describe 'checking if a given handle is valid' do
+      before :each do
+        @handle = '/some_handle'
+        ActionController::Routing::Routes.stubs(:recognize_path).returns({})
+      end
+      
+      it 'should accept a handle' do
+        lambda { Page.valid_handle?(@handle) }.should_not raise_error(ArgumentError)
+      end
+      
+      it 'should require a handle' do
+        lambda { Page.valid_handle? }.should raise_error(ArgumentError)
+      end
+      
+      it 'should ask the routes to recognize the handle' do
+        ActionController::Routing::Routes.expects(:recognize_path).with(@handle).returns({})
+        Page.valid_handle?(@handle)
+      end
+      
+      it 'should return false if the handle recognization is for an existent controller and action' do
+        ActionController::Routing::Routes.stubs(:recognize_path).returns({ :controller => 'something', :action => 'index' })
+        Page.valid_handle?(@handle).should be(false)
+      end
+      
+      it 'should return false if the handle recognization is for the pages controller and has no path' do
+        ActionController::Routing::Routes.stubs(:recognize_path).returns({ :controller => 'pages', :action => 'show', :id => '1' })
+        Page.valid_handle?(@handle).should be(false)
+      end
+      
+      it 'should return true if the handle recognization is for the pages controller and has a path' do
+        ActionController::Routing::Routes.stubs(:recognize_path).returns({ :controller => 'pages', :action => 'show', :path => ['some_handle'] })
+        Page.valid_handle?(@handle).should be(true)
+      end
+      
+      it 'should return true if the handle recognization raises an error' do
+        ActionController::Routing::Routes.stubs(:recognize_path).raises(ActionController::RoutingError.new('No route matches'))
+        Page.valid_handle?(@handle).should be(true)
+      end
+    end
+  end
 end
