67476dd865a05fa32ba7a84f422af5cb3c28a5bb
Yossef Mendelssohn <ymendel@pobox.com>
2010-01-08 16:46:08 -0600

    Making plugin routes the main routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,4 @@ ActionController::Routing::Routes.draw do |map|
   map.namespace :admin do |admin|
     admin.resources :pages
   end
-
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
-
-  map.connect '*path', :controller => 'pages', :action => 'show'
 end
--- lib/plugin-routes.rb
+++
@@ -1,7 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :pages
-
-  map.namespace :admin do |admin|
-    admin.resources :pages
-  end
-end
