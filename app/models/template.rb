c738ff1b1207c407f9ddcd6316c99914904a0c11
Yossef Mendelssohn <ymendel@pobox.com>
2010-02-10 12:46:25 -0600

    Cleaning up Template model and spec.
    Much of the Page stuff is not necessary.

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,30 +1,7 @@
 class Template < AbstractPage
   has_many :pages
+  
   validates_uniqueness_of :handle
-  
-  def validate
-    unless valid_handle?
-      errors.add(:handle, "'#{handle}' is not allowed")
-    end
-  end
-  
-  def valid_handle?
-    self.class.valid_handle?(handle)
-  end
-  
-  class << self
-    def valid_handle?(handle)
-      handle = "/#{handle}" unless (handle || '')[0,1] == '/'
-      recog = ActionController::Routing::Routes.recognize_path(handle)
-      !!(recog[:controller] == 'pages' && recog[:path])
-    rescue ActionController::RoutingError
-      true
-    end
-  end
-  
-  def path
-    "/#{handle}"
-  end
   
   def full_contents(replacements = {})
     self.class.expand(replacements, contents)
--- spec/models/template_spec.rb
+++ spec/models/template_spec.rb
@@ -6,26 +6,16 @@ describe Template do
   end
 
   describe 'attributes' do
-    it 'should have a title' do
-      @template.should respond_to(:title)
-    end
-
-    it 'should allow setting and retrieving the title' do
-      @template.title = 'Test Title'
-      @template.save!
-      @template.reload.title.should == 'Test Title'
-    end
-
     it 'should have a handle' do
       @template.should respond_to(:handle)
     end
-
+    
     it 'should allow setting and retrieving the handle' do
       @template.handle = 'test_handle'
       @template.save!
       @template.reload.handle.should == 'test_handle'
     end
-
+    
     it 'should have contents' do
       @template.should respond_to(:contents)
     end
@@ -44,131 +34,22 @@ describe Template do
       dup.errors.should be_invalid(:handle)
     end
     
-    it 'should check if the handle is valid according to the class' do
+    it 'should not check if the handle is valid according to the class' do
       handle = '/some_crazy_handle'
-      Template.expects(:valid_handle?).with(handle).returns(true)
+      Template.expects(:valid_handle?).never
       Template.generate(:handle => handle)
-    end
-    
-    it 'should require the handle to be valid according to the class' do
-      Template.stubs(:valid_handle?).returns(false)
-      template = Template.generate(:handle => 'some_handle')
-      template.errors.should be_invalid(:handle)
-    end
-    
-    it 'should accept handles that are valid according to the class' do
-      Template.stubs(:valid_handle?).returns(true)
-      template = Template.generate(:handle => 'some_handle')
-      template.errors.should_not be_invalid(:handle)
     end
   end
   
   describe 'associations' do
     it 'can have pages' do
-      Template.new.should respond_to(:pages)
+      @template.should respond_to(:pages)
     end
     
     it 'should allow setting and retrieving pages' do
       template = Template.generate!
       template.pages << page = Page.generate!
       template.pages.should include(page)
-    end
-  end
-  
-  describe 'as a class' do
-    it 'should be able to check if a given handle is valid' do
-      Template.should respond_to(:valid_handle?)
-    end
-    
-    describe 'checking if a given handle is valid' do
-      before :each do
-        @handle = '/some_handle'
-        ActionController::Routing::Routes.stubs(:recognize_path).returns({})
-      end
-      
-      it 'should accept a handle' do
-        lambda { Template.valid_handle?(@handle) }.should_not raise_error(ArgumentError)
-      end
-      
-      it 'should require a handle' do
-        lambda { Template.valid_handle? }.should raise_error(ArgumentError)
-      end
-      
-      it 'should ask the routes to recognize the handle' do
-        ActionController::Routing::Routes.expects(:recognize_path).with(@handle).returns({})
-        Template.valid_handle?(@handle)
-      end
-      
-      it 'should return false if the handle recognization is for an existent controller and action' do
-        ActionController::Routing::Routes.stubs(:recognize_path).returns({ :controller => 'something', :action => 'index' })
-        Template.valid_handle?(@handle).should be(false)
-      end
-      
-      it 'should return false if the handle recognization is for the pages controller and has no path' do
-        ActionController::Routing::Routes.stubs(:recognize_path).returns({ :controller => 'pages', :action => 'show', :id => '1' })
-        Template.valid_handle?(@handle).should be(false)
-      end
-      
-      it 'should return false if the handle recognization is for the pages controller and has a path' do
-        ActionController::Routing::Routes.stubs(:recognize_path).returns({ :controller => 'pages', :action => 'show', :path => ['some_handle'] })
-        Template.valid_handle?(@handle).should be(true)
-      end
-      
-      it 'should return true if the handle recognization raises an error' do
-        ActionController::Routing::Routes.stubs(:recognize_path).raises(ActionController::RoutingError.new('No route matches'))
-        Template.valid_handle?(@handle).should be(true)
-      end
-      
-      it 'should prepend a / to the given handle if needed' do
-        handle = 'something'
-        ActionController::Routing::Routes.expects(:recognize_path).with("/#{handle}").returns({})
-        Template.valid_handle?(handle)
-      end
-      
-      it 'should handle a nil handle' do
-        ActionController::Routing::Routes.expects(:recognize_path).with('/').returns({})
-        Template.valid_handle?(nil)
-      end
-    end
-  
-    it 'should not include snippets when finding' do
-      Template.delete_all
-      Snippet.delete_all
-      
-      # OD doesn't look for exemplars in plugins
-      Template.generate!(:handle => 'template')
-      Snippet.generate!(:handle => 'snippet')
-      
-      Template.count.should == 1
-    end
-  end
-  
-  it 'should be able to check if its handle is valid' do
-    @template.should respond_to(:valid_handle?)
-  end
-  
-  describe 'checking if its handle is valid' do
-    it 'should delegate to the class, passing its own handle' do
-      @template.handle = 'handle'
-      Template.expects(:valid_handle?).with(@template.handle)
-      @template.valid_handle?
-    end
-    
-    it 'should return the result from the class method' do
-      result = 'some result'
-      Template.stubs(:valid_handle?).returns(result)
-      @template.valid_handle?.should == result
-    end
-  end
-  
-  it 'should have a path' do
-    @template.should respond_to(:path)
-  end
-  
-  describe 'path' do
-    it 'should be the handle with a prepended /' do
-      @template.stubs(:handle).returns('some_handle')
-      @template.path.should == '/some_handle'
     end
   end
   
