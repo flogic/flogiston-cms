45c2548723925c8ea8d9cbbf537cbc6422e2ca17
Rick Bradley <rick@rickbradley.com>
2010-02-10 15:02:56 +0530

    Fixing spec problems in installer spec.
    First off, a structural change:  the rails_path and plugin_path test helpers were actually
    silently colliding with the helpers of the same name in the install.rb script under test,
    so renaming this to have test-only scope.
    
    Then, there was an actual test failure exposed (presumably by rails 2.3.5, but perhaps by
    newer mochas) where the branchfor copying in the plugin routes wasn't being accessed under
    test.  Making a stub to trigger this properly.

--- spec/other/install_spec.rb
+++ spec/other/install_spec.rb
@@ -1,11 +1,11 @@
 require File.expand_path(File.join(File.dirname(__FILE__), *%w[.. spec_helper]))
 require 'fileutils'
 
-def plugin_path(relative)
+def test_plugin_path(relative)
   File.expand_path(File.join(File.dirname(__FILE__), '..', '..', relative))
 end
 
-def rails_path(relative)
+def test_rails_path(relative)
   File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', '..', '..', relative))
 end
 
@@ -25,27 +25,28 @@ describe 'the plugin install.rb script' do
   
   describe 'when a RAILS_ROOT/config directory does not exist' do  # it could maybe happen?
     before :each do
-      File.stubs(:directory?).with(rails_path('config')).returns(false)
+      File.stubs(:directory?).with(test_rails_path('config')).returns(false)
+      File.stubs(:file?).with(test_rails_path('config/routes.rb')).returns(false)
     end
     
     it 'should create the RAILS_ROOT/config directory' do
-      FileUtils.expects(:mkdir).with(rails_path('config'))
+      FileUtils.expects(:mkdir).with(test_rails_path('config'))
       do_install
     end
     
     it 'should copy the plugin bottom-routes to the RAILS_ROOT/config/routes.rb file' do
-      FileUtils.expects(:copy).with(plugin_path('lib/plugin-bottom-routes.rb'), rails_path('config/routes.rb'))
+      FileUtils.expects(:copy).with(test_plugin_path('lib/plugin-bottom-routes.rb'), test_rails_path('config/routes.rb'))
       do_install
     end
   end
   
   describe 'when a RAILS_ROOT/config directory exists' do
     before :each do
-      File.stubs(:directory?).with(rails_path('config')).returns(true)
+      File.stubs(:directory?).with(test_rails_path('config')).returns(true)
     end
     
     it 'should not attempt to create the RAILS_ROOT/config directory' do
-      FileUtils.expects(:mkdir).with(rails_path('config')).never
+      FileUtils.expects(:mkdir).with(test_rails_path('config')).never
       do_install
     end
     
@@ -53,7 +54,7 @@ describe 'the plugin install.rb script' do
       before :each do
         @install_rb = File.read(File.join(File.dirname(__FILE__), *%w[.. .. install.rb ]))
         
-        File.stubs(:file?).with(rails_path('config/routes.rb')).returns(true)
+        File.stubs(:file?).with(test_rails_path('config/routes.rb')).returns(true)
         @file = stub('file', :puts => nil)
         File.stubs(:open).yields(@file)
         @routes = 'routes'
@@ -68,12 +69,12 @@ describe 'the plugin install.rb script' do
       end
       
       it 'should open the routes file for appending' do
-        File.expects(:open).with(rails_path('config/routes.rb'), 'a').yields(@file)
+        File.expects(:open).with(test_rails_path('config/routes.rb'), 'a').yields(@file)
         do_install
       end
       
       it 'should read the contents of the plugin bottom-routes file' do
-        File.expects(:read).with(plugin_path('lib/plugin-bottom-routes.rb')).returns(@routes)
+        File.expects(:read).with(test_plugin_path('lib/plugin-bottom-routes.rb')).returns(@routes)
         do_install
       end
       
@@ -85,23 +86,23 @@ describe 'the plugin install.rb script' do
     
     describe 'and the routes file does not exist' do
       before :each do
-        File.stubs(:file?).with(rails_path('config/routes.rb')).returns(false)
+        File.stubs(:file?).with(test_rails_path('config/routes.rb')).returns(false)
       end
       
       it 'should copy the plugin bottom-routes to the RAILS_ROOT/config/routes.rb file' do
-        FileUtils.expects(:copy).with(plugin_path('lib/plugin-bottom-routes.rb'), rails_path('config/routes.rb'))
+        FileUtils.expects(:copy).with(test_plugin_path('lib/plugin-bottom-routes.rb'), test_rails_path('config/routes.rb'))
         do_install
       end
     end
   end
   
   it "should copy the plugin's db migrations to the RAILS_ROOT db/migrate directory" do
-    FileUtils.expects(:cp_r).with(plugin_path('db/migrate'), rails_path('db'))
+    FileUtils.expects(:cp_r).with(test_plugin_path('db/migrate'), test_rails_path('db'))
     do_install
   end
   
   it "should have rails run the plugin installation template" do
-    self.expects(:system).with("rake rails:template LOCATION=#{plugin_path('templates/plugin-install.rb')}")
+    self.expects(:system).with("rake rails:template LOCATION=#{test_plugin_path('templates/plugin-install.rb')}")
     do_install
   end
 
@@ -124,13 +125,13 @@ describe 'the plugin install.rb script' do
 
     it 'should read the plugin README.markdown file' do
       do_install
-      IO.expects(:read).with(plugin_path('README.markdown'))
+      IO.expects(:read).with(test_plugin_path('README.markdown'))
       readme_contents
     end
 
     it 'should return the contents of the plugin README.markdown file' do
       do_install
-      IO.stubs(:read).with(plugin_path('README.markdown')).returns('README CONTENTS')
+      IO.stubs(:read).with(test_plugin_path('README.markdown')).returns('README CONTENTS')
       readme_contents.should == 'README CONTENTS'
     end
   end
