6bf8b8b7623f75b1bac72c310873698aa1de57c5
Yossef Mendelssohn <ymendel@pobox.com>
2011-09-14 13:31:13 -0400

    Adding Templates admin controller
    Pretty much just like the others. You know the drill.

---
+++ app/controllers/admin/templates_controller.rb
@@ -0,0 +1,57 @@
+class Admin::TemplatesController < AdminController
+  def index
+    @templates = Template.all.sort_by(&:handle)
+    @title = 'View all templates'
+  end
+
+  def show
+    @template_obj = Template.find(params[:id])
+    @title = "Viewing template '#{@template_obj.handle}'"
+  end
+
+  def new
+    @template_obj = Template.new
+    @title = 'New template'
+  end
+
+  def create
+    @template_obj = Template.new(params[:template])
+    
+    if !preview? and @template_obj.save
+      redirect_to(admin_template_path(@template_obj))
+    else
+      @title = 'New template'
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @template_obj = Template.find(params[:id])
+    @title = "Editing template '#{@template_obj.handle}'"
+  end
+
+  def update
+    @template_obj = Template.find(params[:id])
+    @template_obj.attributes = params[:template]
+    
+    if !preview? and @template_obj.save
+      redirect_to(admin_template_path(@template_obj))
+    else
+      @title = "Editing template '#{@template_obj.handle}'"
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    template_obj = Template.find(params[:id])
+    template_obj.destroy
+    redirect_to admin_templates_path
+  end
+  
+  
+  private
+  
+  def preview?
+    !params[:preview].blank?
+  end
+end
